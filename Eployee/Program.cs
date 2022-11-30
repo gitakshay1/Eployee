bc37d2b2e6eafd053dff70a046b713b775661514
gitakshay1 <akshaypatil309p8@gmail.com>
2022-11-30 16:25:15 +0530

    [Akshay]-check emp is prsent or absent

--- Eployee/Program.cs
+++ Eployee/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eployee is present/absent");
+        }
+    }
+}//UC1-Eployee present/absent
